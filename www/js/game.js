61139341cafdc55c44ab1bfcd883539c44b18345
EdyPEN <edy2.0andre@gmail.com>
2025-03-24 14:36:38 +0000

    Improved Dungeon Phase

--- www/js/game.js
+++ www/js/game.js
@@ -1,8 +1,6 @@
 function DungeonRequest(){
-    var health = document.getElementById("health").value
-    var energy = document.getElementById("energy").value
-    var insight = document.getElementById("insight").value
-    var damage = document.getElementById("damage").value
+
+
 
     
 }
