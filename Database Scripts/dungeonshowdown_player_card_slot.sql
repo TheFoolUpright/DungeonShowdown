32a9122a2d74c960a199d521cb569b48052d6fc7
TheFoolUpright <mcenteemaryk@gmail.com>
2025-04-05 10:36:45 +0100

    Update table data

--- Database Scripts/dungeonshowdown_player_card_slot.sql
+++ Database Scripts/dungeonshowdown_player_card_slot.sql
@@ -49,9 +49,9 @@ CREATE TABLE `player_card_slot` (
 LOCK TABLES `player_card_slot` WRITE;
 /*!40000 ALTER TABLE `player_card_slot` DISABLE KEYS */;
 INSERT INTO `player_card_slot` VALUES 
-(1,5,1,3,1),
-(2,5,2,1,1),
-(3,5,3,5,1);
+(1,1,1,3,1),
+(2,1,2,1,1),
+(3,1,3,5,1);
 /*!40000 ALTER TABLE `player_card_slot` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
