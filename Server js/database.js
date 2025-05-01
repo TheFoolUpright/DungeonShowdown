39404d9a58c9febd6bd52c8bba58a3a85611efa5
EdyPEN <edy2.0andre@gmail.com>
2025-05-01 13:56:15 +0100

    Change to Database

--- Server js/database.js
+++ Server js/database.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql2")
 
+var hostname = "j31xe.h.filess.io";
+var database = "DungeonShowdown_originalin";
+var port = "61002";
+var username = "DungeonShowdown_originalin";
+var password = "842b203588d10df567b036fe721a06e6e4188006";
+
 const connectionOptions = mysql.createConnection({
-    host: "localhost", 
-    user: "root", 
-    password: "master1234", 
-    database: "dungeonshowdown"
+    host: hostname,
+    user: username,
+    password,
+    database,
+    port,
 });
 
 module.exports = connectionOptions;
