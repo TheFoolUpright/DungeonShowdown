7f7d1d4f7d5700d43656aa4a6e1c4dc12a90606f
EdyPEN <edy2.0andre@gmail.com>
2025-05-15 23:31:08 +0100

    Showdown Cards

--- www/Game/src/scenes/Preload.js
+++ www/Game/src/scenes/Preload.js
@@ -60,7 +60,7 @@ class Preload extends Phaser.Scene {
 
 		this.editorPreload();
 
-		this.load.on(Phaser.Loader.Events.COMPLETE, () => this.scene.start("Dungeon"));
+		this.load.on(Phaser.Loader.Events.COMPLETE, () => this.scene.start("Showdown"));
 	}
 
 	/* END-USER-CODE */
