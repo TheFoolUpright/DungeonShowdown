8112eb73464ee527d8c6291e6a77611c5cbf0612
EdyPEN <edy2.0andre@gmail.com>
2025-10-06 13:51:03 +0100

    6/10 test

--- api/database.js
+++ api/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2")
 
-var hostname = "j31xe.h.filess.io";
-var database = "DungeonShowdown_originalin";
-var port = "61002";
-var username = "DungeonShowdown_originalin";
-var password = "842b203588d10df567b036fe721a06e6e4188006";
+var hostname = "u7knk5.h.filess.io";
+var database = "DungeonShowdown_caretubeat";
+var port = "61032";
+var username = "DungeonShowdown_caretubeat";
+var password = "cb5297275b6b5a97e719d421b1440160c8cece7a";
 
 // var hostname = "localhost";
 // var database = "dungeonshowdown";
