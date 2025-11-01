ea0d1fdfcc5dee8459db45341409c1fa9664bb47
TheFoolUpright <mcenteemaryk@gmail.com>
2025-11-01 09:28:33 +0000

    Update database connection

--- api/database.js
+++ api/database.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql2")
 
-var hostname = "u7knk5.h.filess.io";
-var database = "DungeonShowdown_caretubeat";
-var port = "61032";
-var username = "DungeonShowdown_caretubeat";
-var password = "cb5297275b6b5a97e719d421b1440160c8cece7a";
+// var hostname = "u7knk5.h.filess.io";
+// var database = "DungeonShowdown_caretubeat";
+// var port = "61032";
+// var username = "DungeonShowdown_caretubeat";
+// var password = "cb5297275b6b5a97e719d421b1440160c8cece7a";
 
-// var hostname = "localhost";
-// var database = "dungeonshowdown";
-// var port = "3306";
-// var username = "root";
-// var password = "master1234";
+var hostname = "localhost";
+var database = "dungeonshowdown";
+var port = "3306";
+var username = "root";
+var password = "master1234";
 
 const connectionOptions = mysql.createConnection({
     host: hostname,
